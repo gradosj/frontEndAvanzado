2f2ae890357c3f84d2df598a652f5ef4550c1cb0
Jose Antonio Grados Barriga <gradosj@gmail.com>
2020-01-26 18:20:31 +0100

    funciona

--- src/js/details.js
+++ src/js/details.js
@@ -1,43 +1,16 @@
 
 const API_KEY = 'V0XRE4Q-FTYMPCA-MDWV1J2-XCFC55F';
 const detail = document.querySelector('#detalle');
+let pintado = false;
 
-/*const beerId = 5;*/
-/*
-const detailTeplate = ({beerId, name, description, image} = {}) => {
-  console.log('entra en detail');
-return `
-<div class="card mb-3" style="max-width: 540px;">
-        <div class="row no-gutters">
-          <div class="col-md-4">
-            <img src="https://images.punkapi.com/v2/58.png" class="card-img" alt="...">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-          </div>
-        </div>
-      </div>
-`;
-};*/
-/*
-const renderDetail = async beerId => {
-  try {
-     detail = await downloadDetails(beerId);
-  }
-  catch (err) {
-    // manejo de errores
-    console.log('errores');
-  }
-}
-*/
 /*detail.addEventListener('click', evt => {
 
   downloadsDetails(beerId);*/
   function downloadsDetails(beerId) {
+    pintado = true;
+    console.log('pintado: ',pintado)
+
+
 
     let api = `https://beerflix-api.herokuapp.com/api/v1/beers/${beerId}`;
     console.log(api);
@@ -54,13 +27,12 @@ const renderDetail = async beerId => {
   }
 
   function imprimirHTML(datos) {
+
+    
     console.log('entra en imprimir html: ' , datos);
     typeof(datos);
-   
-    //datos.forEach(cervezas => {
-      
 
-      const { name, image, firstBrewed, beerId, description } = datos;
+      const { name, image, firstBrewed, beerId, description, likes } = datos;
              
         let cards = document.querySelector('#show-section');
         cards.innerHTML =  `
@@ -74,19 +46,52 @@ const renderDetail = async beerId => {
               <h5 class="card-title">id:${beerId} / ${name}</h5>
               <p class="card-text">${firstBrewed}</p>
               <p class="card-text">${description}</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              <p id = "imgLikes" class="card-text"><small class="text-muted">LIKE! ${likes}</small></p>
             </div>
           </div>
         </div>
       </div>
           
           `;
-     
+
+          const imgLikes = document.querySelector('#imgLikes');
+          imgLikes.addEventListener('click', evt => {
+        
+            pushLikes(beerId);
+        
+            function pushLikes(beerId) {
       
-    //})
+                let api = `https://beerflix-api.herokuapp.com/api/v1/beers/${beerId}/like`;
+               
+        
+                return fetch(api, {
+                    method: 'POST',
+                    headers: {
+                        'X-API-KEY': API_KEY,
+                    }
+                })
+                    .then(respuesta => respuesta.json())
+                    .then(datos => imprimirHTML(datos.beer))
+                /*  .then(datos => console.log(datos.beers))*/
+            }
+        
+            function imprimirHTML(datos) {
+              console.log('bla bla' , datos);
+              
+              const { likes } = datos;
+                            let ponLikes = document.querySelector('#imgLikes');
+                            ponLikes.innerHTML =  `
+                            <p id = "imgLikes" class="card-text"><small class="text-muted">LIKE! ${likes}</small></p>
+                                    
+                    `;
+                      
+                    }
+                })
   }
   
-  
+   
+
+
 /*
 });
 */
---
+++ src/js/likes.js
@@ -0,0 +1,1 @@
+console.log('windows.location : ', window.location.href);
